4689f72496bb80173fd89a581bc42c9c5264ab0f
Main user and administrator of this system <madnificent@Freyr.snake>
2008-05-16 12:27:57 +0200

    Basic system without generate helpers

--- tasks/tags_done_right_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :tags_done_right do
-#   # Task goes here
-# end
--- test/tags_done_right_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-
-class TagsDoneRightTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
-end
