f6023907f2f1d5aa5d64c04fa587d9bf8bfb0323
MADnificent <madnificent@gmail.com>
2008-08-27 23:41:36 +0200

    Tag clouds can now be generated from a Model, whilst only counting those instances of the model that have all the given tags.
    This means that Shop.tags_cloud( :tags => [InternalTag.find_by_name cheap] ) will return a tag_cloud whilst counting only those shops that have the tag 'cheap' connected to it.
    
    To keep the speed up to par, the calculations are done in one database-query.  Do take into account that this is a rather complicated query, so it may still be usefull to cache this for heavy loads.

--- lib/tags_done_right.rb
+++ lib/tags_done_right.rb
@@ -26,8 +26,13 @@ module TagsDoneRight
   module ClassMethods
     def acts_as_taggable( opts={ })
       opts = { :tag_class => Tag, :tag_names_name => "tag_names", :show_separator => ", ", :read_separator => /[,\.\s;]+/ }.merge opts
-      
-      
+
+      connection_table_name = if self.table_name < opts[:tag_class].table_name
+                                self.table_name + "_" + opts[:tag_class].table_name
+                              else
+                                opts[:tag_class].table_name + "_" + self.table_name
+                              end
+
       # Link the Tag class to the current class, and vice-versa
       has_and_belongs_to_many opts[:tag_class].table_name
 
@@ -46,22 +51,58 @@ module TagsDoneRight
       end
       
       self.class.send! "define_method", "#{opts[:tag_class].to_s.tableize}_cloud" do |options|
-        method_opts = { :items => 10 , :groups => nil }.merge options
+        method_opts = { :items => 10 , :groups => nil , :tags => [] }.merge options
         method_opts[:groups] ||= method_opts[:items]
-        
-        cloud = opts[:tag_class].find( :all,
-                                       :select => "*, COUNT(*) AS tag_count",
-                                       :joins =>  self.table_name.to_sym,
-                                       :group =>  opts[:tag_class].to_s.table_name.singularize + "_id",
-                                       :order => "tag_count DESC",
-                                       :limit => method_opts[:items] )
+        tags = method_opts[:tags]
+        items = method_opts[:items]
+        groups = method_opts[:groups]
 
-        max = cloud.first.tag_count.to_i if cloud.first
-        cloud.each do |tag|
-          tag.tag_count = ((tag.tag_count.to_i * method_opts[:groups]) / max).to_i
+        if tags and not tags.empty?
+          # query contains an insane query that fetches the tag, and a count for that tag
+          query = "
+SELECT TAG.*, COUNT(*) AS tag_count
+FROM (SELECT * 
+      FROM #{connection_table_name} TAGS_MODELS
+           JOIN #{self.table_name} MODEL
+           ON MODEL.id = TAGS_MODELS.#{self.table_name.singularize + '_id'}
+      WHERE "
+          and_sql = "" # this is replaced by and after the first tag
+          tags.each do |tag|
+            query += and_sql + "
+            EXISTS (SELECT * 
+                    FROM #{connection_table_name} SHS, #{opts[:tag_class].table_name} TAG
+                    WHERE MODEL.id = SHS.#{self.table_name.singularize + '_id'}
+                          AND TAG.name = '#{tag.name}'
+                          AND SHS.#{opts[:tag_class].table_name.singularize + '_id'} = TAG.id)"
+            and_sql = " AND "
+          end
+          query +=                                         ") AS MEGA
+     JOIN #{opts[:tag_class].table_name} TAG
+     ON TAG.id = MEGA.#{opts[:tag_class].table_name.singularize + '_id'}
+GROUP BY TAG.id
+ORDER BY tag_count DESC
+LIMIT #{items}"
+
+          cloud = InternalTag.find_by_sql( query )
+          max = cloud.first.tag_count.to_i if cloud.first
+          cloud.each do |tag|
+            tag.tag_count = ((tag.tag_count.to_i * groups) / max).to_i
+          end
+
+          # remove the tags that are given
+          # Note: this reduces the maximum amount of given tags for clouds to max - 1 (hooray for caching)
+          tags.each do |tag|
+            cloud.delete tag
+          end
+          cloud
+        else
+          cloud = opts[:tag_class].find( :all,
+                                         :select => "*, COUNT(*) AS tag_count",
+                                         :joins =>  self.table_name.to_sym,
+                                         :group =>  opts[:tag_class].table_name.singularize + "_id",
+                                         :order => "tag_count DESC",
+                                         :limit => method_opts[:items] )
         end
-        
-        cloud
       end
       
       extend TagsDoneRight::SingletonMethods
